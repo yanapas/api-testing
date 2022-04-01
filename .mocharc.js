edb9ded138097c624184912beb5ab4e889a5bc16
Iana Pastukhova <monstervan221188@gmail.com>
2022-03-31 18:15:09 -0700

    add before hooks for auth spec add root before hook

--- .mocharc.js
+++ .mocharc.js
@@ -1,5 +1,7 @@
 module.exports = {
     require: '@babel/register',
     spec: 'specs/**/*.spec.js',
-    ignore: 'specs/example.spec.js'
+    ignore: 'specs/example.spec.js',
+    file: 'config/setup.js',
+
 }
