d9bd8f1a567c50217340abe6d1f7d4b0bf4407f4
Iana Pastukhova <monstervan221188@gmail.com>
2022-04-04 21:08:49 -0700

    add auth helper

--- specs/auth.spec.js
+++ specs/auth.spec.js
@@ -1,48 +1,33 @@
-import supertest from 'supertest';
-import { expect } from 'chai';
-
+import { expect } from 'chai'
+import AuthHelper from '../helpers/auth.helper'
 
 describe('Auth', () => {
-    const request = supertest(process.env.BASE_URL)
+    let authHelper = new AuthHelper()
 
     describe('Successful log in', () => {
-        let result
-
         before(async function() {
-            await request
-                .post('/auth')
-                .send({ login: process.env.LOGIN, password: process.env.PASSWORD})
-                .then(res => {
-                 result = res
-                })
+            await authHelper.login(process.env.LOGIN, process.env.PASSWORD)
         })
 
         it('Response status code is 200', () => {
-            expect(result.statusCode).to.eq(200)
+            expect(authHelper.response.statusCode).to.eq(200)
         })
 
         it('Response body contains authorization token', () => {
-            expect(result.body.token).not.to.be.undefined
+            expect(authHelper.response.body.token).not.to.be.undefined
         })
 
     })
     describe('Log in with invalid credentials', () => {
-        let result
-
         before(async function() {
-            await request
-                .post('/auth')
-                .send({ login: 'invalid', password: 'invalid'})
-                .then(res => {
-                    result = res
-                })
+            await authHelper.login('invalid', 'invalid')
         })
 
         it('Response status code is 404', () => {
-                    expect(result.statusCode).to.eq(404)
+                    expect(authHelper.response.statusCode).to.eq(404)
                 })
         it('Response body contains error message', () => {
-                    expect(result.body.message).to.eq('Wrong login or password.')
+                    expect(authHelper.response.body.message).to.eq('Wrong login or password.')
         })
     })
 
